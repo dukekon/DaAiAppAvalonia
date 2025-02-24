34f0aee05ec8d938f52a8d7383d3a0213e7b2a08
Duke <19833038@qq.com>
2025-02-24 16:15:06 +0800

    创建项目

---
+++ DaAiApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DaAiApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ DaAiApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DaAiApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
